d2ecec764a48ca3ba06bfc55843fcb439f925636
nazisharifi2025 <banonazisharifi@gmail.com>
2025-07-03 20:48:36 +0430

    l

---
+++ src/Componints/Frout.jsx
@@ -0,0 +1,33 @@
+import SingelFrouts from "./SingelFrout";
+function frouts(){
+//    const animals =[
+//         {
+//             name : "Cat",
+//             emoji : "😺",
+//             price : "120"
+//         },
+//         {
+//             name : "Dog",
+//             emoji : "🐶",
+//             price : "2000"
+//         },
+//         {
+//             name : "Monky",
+//             emoji : "🙊",
+//             price : "190"
+//         },
+//         {
+//             name : "Mouse",
+//             emoji : "🐭",
+//             price : "90"
+//         },
+//     ]
+//     return(
+//         <div className="h-screen bg-pink-400 text-white  flex justify-center items-center w-full">
+//             {animals.map(SingelAnimal=>(
+//                 <SingelAnimal/>
+//             ))}
+//         </div>
+//     )
+// }
+// export default Animals;
