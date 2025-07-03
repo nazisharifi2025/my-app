4e1fb103deb746e3701643fe5a6f6a1c161e8d66
nazisharifi2025 <banonazisharifi@gmail.com>
2025-07-03 10:05:00 +0430

    l

--- src/Componints/Animals.jsx
+++ src/Componints/Animals.jsx
@@ -1,6 +1,29 @@
 function Animals(){
+    Animals =[
+        {
+            name : "Cat",
+            emoji : "😺",
+            price : "120"
+        },
+        {
+            name : "Dog",
+            emoji : "🐶",
+            price : "2000"
+        },
+        {
+            name : "Monky",
+            emoji : "🙊",
+            price : "190"
+        },
+        {
+            name : "Mouse",
+            emoji : "🐭",
+            price : "90"
+        },
+    ]
     return(
         <>
+
         </>
     )
 }
