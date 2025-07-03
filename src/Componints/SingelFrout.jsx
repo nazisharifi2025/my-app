8e16c85e75ddd4429fd6ef0fce7a70ca51cfff61
nazisharifi2025 <banonazisharifi@gmail.com>
2025-07-03 20:45:31 +0430

    l

--- src/Componints/SingelFrout.jsx
+++ src/Componints/SingelFrout.jsx
@@ -1,9 +1,10 @@
 function SingelFrouts(){
     return(
         <div>
-            <h1></h1>
-            <h1></h1>
-            <h1></h1>
+            <h1>🐶</h1>
+            <h1>Dog</h1>
+            <h1>300</h1>
         </div>
     )
 }
+// export default SingelFrouts;
