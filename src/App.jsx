130b17247aa07b01a11c113af4b8ae4dd4e8d448
nazisharifi2025 <banonazisharifi@gmail.com>
2025-07-04 12:17:35 +0430

    l

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
 import Animals from "./Componints/Animals";
 import Frout from "./Componints/Frout";
 function App(){
-  let natiga = false;
-  if (natiga) {
-     return(
-    <div className="w-full  flex justify-center flex-col">
-      <Animals />
-    </div>
-  )
-  }
-   else{
-       return(
-    <div className="w-full  flex justify-center flex-col">
-      <Frout/>
-    </div>
-  )
-   }
+  let natiga = "Frout";
+  return natiga ==="Animal"? <Animals/> : <Frout/>
+  // if (natiga) {
+  //    return(
+  //   <div className="w-full  flex justify-center flex-col">
+  //     <Animals />
+  //   </div>
+  // )
+  // }
+  //  else{
+  //      return(
+  //   <div className="w-full  flex justify-center flex-col">
+  //     <Frout/>
+  //   </div>
+  // )
+  //  }
 }
 export default App;
