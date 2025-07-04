8791bdde1bc23ed1e21b822a8ea3e78c6e3fdd45
nazisharifi2025 <banonazisharifi@gmail.com>
2025-07-04 11:16:54 +0430

    l

--- src/Componints/SingelAnimal.jsx
+++ src/Componints/SingelAnimal.jsx
@@ -1,6 +1,6 @@
 function SingelAnimal(){
     return(
-        <div className="P-4 flex flex-col bg-white shadow-md shadow-gray-700 rounded-2xl text-black">
+        <div className="P-4 flex flex-col justify-center items-center bg-white shadow-md shadow-gray-700 rounded-2xl text-black">
             <h1>🍎</h1>
             <h1>Apple</h1>
             <h1>180</h1>
