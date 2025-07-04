dfd7e9cc3d4c33b7c0f3ac754b19ad92f5326603
nazisharifi2025 <banonazisharifi@gmail.com>
2025-07-04 12:12:53 +0430

    l

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import Animals from "./Componints/Animals";
+import Frout from "./Componints/Frout";
 function App(){
-  return(
-    <div className="w-full  flex justify-center">
+  let natiga = false;
+  if (natiga) {
+     return(
+    <div className="w-full  flex justify-center flex-col">
       <Animals />
     </div>
   )
+  }
+   else{
+       return(
+    <div className="w-full  flex justify-center flex-col">
+      <Frout/>
+    </div>
+  )
+   }
 }
 export default App;
