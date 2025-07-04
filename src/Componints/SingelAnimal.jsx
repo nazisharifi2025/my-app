3bccab13465fa980b1f3d188afc0c87bbecd04fb
nazisharifi2025 <banonazisharifi@gmail.com>
2025-07-04 11:15:49 +0430

    l

--- src/Componints/SingelAnimal.jsx
+++ src/Componints/SingelAnimal.jsx
@@ -1,6 +1,6 @@
 function SingelAnimal(){
     return(
-        <div className="P-4 bg-white shadow-md shadow-gray-700">
+        <div className="P-4 bg-white shadow-md shadow-gray-700 rounded-2xl text-black">
             <h1>🍎</h1>
             <h1>Apple</h1>
             <h1>180</h1>
