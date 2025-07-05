98723bb45a4111056eb9dd5387bfe7d9b25146fe
nazisharifi2025 <banonazisharifi@gmail.com>
2025-07-05 22:24:53 +0430

    l

--- src/Componints/anim.jsx
+++ src/Componints/anim.jsx
@@ -1,33 +1,8 @@
-import SingelAnimal from "./SingelAnimal";
-function Animals(){
-   const animals =[
-        {
-            name : "Cat",
-            emoji : "😺",
-            price : "120"
-        },
-        {
-            name : "Dog",
-            emoji : "🐶",
-            price : "2000"
-        },
-        {
-            name : "Monky",
-            emoji : "🙊",
-            price : "190"
-        },
-        {
-            name : "Mouse",
-            emoji : "🐭",
-            price : "90"
-        },
-    ]
+function Anim(){
     return(
-        <div className="h-screen bg-pink-400 justify-between px-3 text-white  flex items-center w-full">
-//             {animals.map(item=> (
-                <SingelAnimal name={item.name} emoji={item.emoji} price={item.price}/>
-            ))}
+        <div>
+
         </div>
     )
 }
-export default Animals;
+export default Anim;
