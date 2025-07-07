32dfe135a750f13c0a53f20ad8d2501d74248a43
nazisharifi2025 <banonazisharifi@gmail.com>
2025-07-07 22:05:58 +0430

    l'

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,20 @@ import Animals from "./Componints/Animals";
 import Frout from "./Componints/Frout";
 function App(){
   let natiga = "Frout";
-  return natiga ==="Animal"? <Animals/> : <Frout/>
-  // if (natiga) {
-  //    return(
-  //   <div className="w-full  flex justify-center flex-col">
-  //     <Animals />
-  //   </div>
-  // )
-  // }
-  //  else{
-  //      return(
-  //   <div className="w-full  flex justify-center flex-col">
-  //     <Frout/>
-  //   </div>
-  // )
-  //  }
+  // return natiga ==="Animal"? <Animals/> : <Frout/>
+  if (natiga==="Animals") {
+     return(
+    <div className="w-full  flex justify-center flex-col">
+      <Animals />
+    </div>
+  )
+  }
+   else{
+       return(
+    <div className="w-full  flex justify-center flex-col">
+      <Frout/>
+    </div>
+  )
+   }
 }
 export default App;
--- src/Componints/anim.jsx
+++
@@ -1,9 +0,0 @@
-function Anim(){
-    return(
-        <div className="h-screen w-full bg-linear-180 from-pink-400 to-blue-500 via-green-500 flex-col gap-6 flex justify-center ">
-            <h1 className="font-bold text-2xl text-center">Hi</h1>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae aspernatur optio dolore perspiciatis. Ea, a? Atque ullam inventore eum quos error necessitatibus modi dignissimos ut quae. Ipsa minima est quam!</p>
-        </div>
-    )
-}
-export default Anim;
