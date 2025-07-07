5edb045f9e1fa75cde9b205c20089f35899cba13
nazisharifi2025 <banonazisharifi@gmail.com>
2025-07-07 22:20:22 +0430

    l

--- src/anima.jsx
+++ src/anima.jsx
@@ -25,8 +25,8 @@ function Anima(){
  
     return (
           <div className=" w-full h-screen bg-pink-400 justify-between px-3 text-white  flex items-center">
-            {/* {animals.map(item=> (
-                <SingelAnimal name={item.name} emoji={item.emoji} price={item.price}/>
+            { {animals.map(item=> (
+              /*  <SingelAnimal name={item.name} emoji={item.emoji} price={item.price}/>
             ))}
         </div> */}
     )
