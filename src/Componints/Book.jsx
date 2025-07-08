902a231c2e1108ad78e993606bee29bede67cd2a
nazisharifi2025 <banonazisharifi@gmail.com>
2025-07-08 22:51:42 +0430

    l

--- src/Componints/Book.jsx
+++ src/Componints/Book.jsx
@@ -1,5 +1,5 @@
 export default function Book(){
     return (
-        <div className="h-screen w-full"></div>
+        <div className="h-screen w-full bg-gray-400 flex justify-between"></div>
     )
 }
