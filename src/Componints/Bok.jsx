ae1467bf22e294fcf32b8d1e4954f52d3fc5fc61
nazisharifi2025 <banonazisharifi@gmail.com>
2025-07-09 22:42:12 +0430

    l

--- src/Componints/Bok.jsx
+++ src/Componints/Bok.jsx
@@ -1,6 +1,6 @@
 export default function Bok({name , imgUrl , price}){
     return(
-        <div className="h-full w-full">
+        <div className="h-full w-full bg-black shadow-md">
         <h1>{name}</h1>
         <img src={imgUrl} alt="" />
         <h1>{price}</h1>
