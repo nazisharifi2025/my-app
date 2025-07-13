095f5eacd6953ad8a52658a66705998befb35e2e
nazisharifi2025 <banonazisharifi@gmail.com>
2025-07-13 09:13:42 +0430

    l

--- src/Componints/Bok.jsx
+++ src/Componints/Bok.jsx
@@ -1,9 +1,9 @@
 export default function Bok({name , imgUrl , price}){
     return(
-        <div className="h-[350px] w-[330px]  bg-black shadow-md rounded-2xl shadow-gray-500 items-center  text-white flex justify-between">
-        <h1>{name}</h1>
-        <img src={imgUrl} alt="" />
+        <div className="h-[350px] w-[330px] flex-col bg-gray-200 text-black shadow-md rounded-2xl shadow-gray-500 items-center   flex justify-center">
+        <h1 className="font-bold text-2xl text-shadow-gray-800">{name}</h1>
         <h1>{price}</h1>
+        <img src={imgUrl} alt="" /> 
         </div>
     )
 }
--- src/Componints/Book.jsx
+++ src/Componints/Book.jsx
@@ -1,9 +1,9 @@
-import bad from "../img/bad.jpg";
-import fran from "../img/fran.jpg";
-import bland from "../img/bland.jpg";
-import talar from "../img/talar.jpg";
-import muhakima from "../img/muhakima.jpg";
-import salar from "../img/salar.jpg";
+import bad from "../assets/img/bad.jpg";
+import fran from "../assets/img/fran.jpg";
+import bland from "../assets/img/bland.jpg";
+import talar from "../assets/img/talar.jpg";
+import muhakima from "../assets/img/muhakima.jpg";
+import salar from "../assets/img/salar.jpg";
 import Bok from "./Bok"
 export default function Book(){
      const book =[
@@ -13,36 +13,36 @@ export default function Book(){
             price : 180
         },
          {
-            name : "اثر مرکب",
+            name : "فرانگشتان",
             imgUrl : fran,
-            price : 200,
+            price : 200
         },
          {
-            name : "قانون پنح ثانیه",
+            name : "بادبدک باز",
             imgUrl : bad,
-            price : 150,
+            price : 150
         },
          {
-            name : "جنون",
+            name : "تالار گرگ",
             imgUrl : talar,
-            price : 100,
+            price : 100
         },
           {
             name : "محاکمه",
             imgUrl : muhakima,
-            price : 180,
+            price : 180
         },
           {
-            name : "سالار",
+            name : "سالار مگس ها",
             imgUrl : salar,
-            price : 170,
+            price : 170
         },
         ]
     return (
        
         <div className="h-screen w-full bg-gray-400 flex justify-between gap-6 overflow-y-scroll p-6 flex-wrap font-bold">
             {book.map(item=>(
-                <Bok name ={book.name} imgUrl = {book.imgUrl} price = {book.price}/>
+                <Bok name ={item.name} imgUrl = {item.imgUrl} price = {item.price}/>
             ))}
         </div>
     )
