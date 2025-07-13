bd2aaa914ded34b9578fcb2268ad14ffd25bf4b1
nazisharifi2025 <banonazisharifi@gmail.com>
2025-07-13 10:45:55 +0430

    l

--- src/Componints/Counter.jsx
+++ src/Componints/Counter.jsx
@@ -1,5 +1,14 @@
+import { useState } from "react";
+
 export default function Counter(){
     let number = 0 ;
+    const [countUp , counter] = useState(0);
+    function Incermint(){
+        counter(countUp + 1);
+    }
+    function Decrimint(){
+        counter (countUp - 1);
+    }
     return(
         <div >
             <h1 className="w-full bg-gray-300 flex justify-center items-center py-6 font-bold text-2xl">the number is : {number}</h1>
