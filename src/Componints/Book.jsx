85f01f6087ee103fdb0a347d03a226c9fb353182
nazisharifi2025 <banonazisharifi@gmail.com>
2025-07-13 09:22:16 +0430

    l

--- src/Componints/Book.jsx
+++ src/Componints/Book.jsx
@@ -13,7 +13,7 @@ export default function Book(){
             price : 180
         },
          {
-            name : "فرانگشتان",
+            name : "فرانکنشتاین",
             imgUrl : fran,
             price : 200
         },
@@ -40,7 +40,7 @@ export default function Book(){
         ]
     return (
        
-        <div className="h-screen w-full bg-gray-400 flex justify-between gap-6 overflow-y-scroll p-6 flex-wrap font-bold">
+        <div className="h-screen w-full bg-gray-300 flex justify-between gap-6 overflow-y-scroll p-6 flex-wrap font-bold">
             {book.map(item=>(
                 <Bok name ={item.name} imgUrl = {item.imgUrl} price = {item.price}/>
             ))}
