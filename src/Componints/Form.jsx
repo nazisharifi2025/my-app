7b098c5ea83e9d822625f1cc0b51b1fc6ee51003
nazisharifi2025 <banonazisharifi@gmail.com>
2025-07-17 21:12:16 +0430

    l

--- src/Componints/Form.jsx
+++ src/Componints/Form.jsx
@@ -2,7 +2,7 @@ export default function(){
     return(
         <div className="h-screen w-full bg-gray-600 flex justify-center items-center">
             <form action="">
-                <input type="text" name="" id="" className="px-6 py-4" />
+                <input type="text" name="" id="" className="px-6 py-4 border border-black" />
             </form>
         </div>
     )
