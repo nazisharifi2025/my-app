54fca91c586b284052045bc33f89a495e5d63be2
nazisharifi2025 <banonazisharifi@gmail.com>
2025-07-17 21:10:56 +0430

    l

--- src/Componints/Form.jsx
+++ src/Componints/Form.jsx
@@ -1,5 +1,9 @@
 export default function(){
     return(
-        <div className="h-screen w-full bg-gray-600 flex justify-center items-center"></div>
+        <div className="h-screen w-full bg-gray-600 flex justify-center items-center">
+            <form action="">
+                <input type="text" name="" id="" />
+            </form>
+        </div>
     )
 }
