cbdbb47e4cdaa6863035644812438f6cd9c80c78
nazisharifi2025 <banonazisharifi@gmail.com>
2025-07-17 21:18:11 +0430

    l

--- src/Componints/Form.jsx
+++ src/Componints/Form.jsx
@@ -2,8 +2,8 @@ export default function(){
     return(
         <div className="h-screen w-full bg-gray-600 flex justify-center items-center">
             <form action="">
-                <input type="text" name="Name" value={} onChange={(e)=>{}} className="px-6 py-4 border  border-black" />
-                <button className="p-2 border">Save</button>
+                <input type="text" name="Name" value={} onChange={(e)=>{}} className="px-6 border-r-0 py-4 border  border-black" />
+                <button className="p-2 border border-l-0">Save</button>
             </form>
         </div>
     )
