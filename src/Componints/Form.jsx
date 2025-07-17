22a3688b517df85c9f55cf0b971399030f4b60c4
nazisharifi2025 <banonazisharifi@gmail.com>
2025-07-17 21:36:00 +0430

    l

--- src/Componints/Form.jsx
+++ src/Componints/Form.jsx
@@ -5,7 +5,7 @@ export default function(){
     return(
         <div className="h-screen w-full bg-gray-600 flex justify-center items-center">
             <form >
-                <input type="text" name="Name"  onChange={(e)=>{}} className="px-6 rounded-md outline-0 rounded-r-none border-r-0 py-4 border-2  border-black" />
+                <input type="text" name="Name" value={todo}  onChange={(e)=>{settodo(e.target.value)}} className="px-6 rounded-md outline-0 rounded-r-none border-r-0 py-4 border-2  border-black" />
                 <button className="p-4 border-2 border-l-0 bg-gray-300 font-bold rounded-md rounded-l-none">Save</button>
             </form>
         </div>
