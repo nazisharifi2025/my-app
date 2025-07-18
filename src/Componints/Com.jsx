dfa9f4a02f9435bedd28812c80a63291896fa1d0
nazisharifi2025 <banonazisharifi@gmail.com>
2025-07-18 20:36:47 +0430

    l

--- src/Componints/Com.jsx
+++ src/Componints/Com.jsx
@@ -1,5 +1,5 @@
 export default function (){
     return(
-        <div className="h-screen w-full bg-linear to-gray-800 from-15% from-gray-400 to-20% rounded-2xl p-2 m-5 flex items-center justify-center gap-3 font-bold"></div>
+        <div className="h-screen w-full bg-linear to-gray-800 via-45% via-gray-400 from-15% from-gray-400 to-20% rounded-2xl p-2 m-5 flex items-center justify-center gap-3 font-bold"></div>
     )
 }
