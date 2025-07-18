d5049df32321978945384ee2629af405fee48510
nazisharifi2025 <banonazisharifi@gmail.com>
2025-07-18 20:18:57 +0430

    l

--- src/Componints/Form.jsx
+++ src/Componints/Form.jsx
@@ -13,7 +13,8 @@ export default function(){
                 <input type="text" name="Name" value={todo}  onChange={(e)=>{settodo(e.target.value)}} className="px-6 rounded-md outline-0 rounded-r-none border-r-0 py-4 border-2  border-black" />
                 <button className="p-4 border-2 border-l-0 bg-gray-300 font-bold rounded-md rounded-l-none">Save</button>
             </form>
-            {console.log(todos)}
+            {/* {console.log(todos)} */}
+            {todos.map(item=>)}
         </div>
     )
 }
