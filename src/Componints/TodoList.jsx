2d79c4498143c40e6801e509bdf599e222337d78
nazisharifi2025 <banonazisharifi@gmail.com>
2025-07-19 21:53:53 +0430

    l

--- src/Componints/TodoList.jsx
+++ src/Componints/TodoList.jsx
@@ -1,4 +1,4 @@
-export default function (){
+export default function TodoList(){
     return(
         <div className="h-20 flex w-full justify-center items-center bg-gray-500 shadow-2xl shadow-gray-900 text-2xl font-bold text-center ">
             <h1 className="text-2xl">Login Form</h1>
