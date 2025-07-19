844d52b75e5f0a2c08dc437fc4e4f8500248de1d
nazisharifi2025 <banonazisharifi@gmail.com>
2025-07-19 21:50:45 +0430

    l

--- src/Componints/TodoList.jsx
+++ src/Componints/TodoList.jsx
@@ -1,5 +1,5 @@
 export default function (){
     return(
-        <div className="h-screen w-full bg-gray-500 shadow-2xl shadow-gray-900 text-2xl text-center"></div>
+        <div className="h-20 flex w-full bg-gray-500 shadow-2xl shadow-gray-900 text-2xl text-center "></div>
     )
 }
