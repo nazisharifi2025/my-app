9f4d0fc68ccddd942087e346dba329d130572f4e
nazisharifi2025 <banonazisharifi@gmail.com>
2025-07-19 21:47:37 +0430

    l

--- src/Componints/Form.jsx
+++ src/Componints/Form.jsx
@@ -16,9 +16,7 @@ export default function(){
             {todos.map(item=>{
                return <h1 className="bg-white px-3 py-2 rounded-md text-2xl font-bold text-center mx-auto" key={item}>{item}</h1>
             })}
-            {
-                console.log(todos)
-            }
+          
         </div>
     )
 }
