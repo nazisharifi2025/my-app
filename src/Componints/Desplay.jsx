3ae6328bd1bbd176320f68847c2a47b3ea8d7310
nazisharifi2025 <banonazisharifi@gmail.com>
2025-07-20 22:06:12 +0430

    l

--- src/Componints/Desplay.jsx
+++ src/Componints/Desplay.jsx
@@ -2,6 +2,7 @@ export default function Desplay(){
     return(
         <div className="w-full h-fit bg-amber-800 text-2xl text-white flex justify-between items-center font-bold">
             <div className="font-bold text-center">Count in 0</div>
+            <div className="font-bold text-center">Count in 0</div>
         </div>
     )
 }
