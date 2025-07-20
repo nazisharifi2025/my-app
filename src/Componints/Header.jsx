20ec816dcd58f5fc15167313fdec2b0c0690292e
nazisharifi2025 <banonazisharifi@gmail.com>
2025-07-20 22:09:51 +0430

    l

--- src/Componints/Header.jsx
+++ src/Componints/Header.jsx
@@ -1,5 +1,5 @@
 export default function Header(){
     return(
-        <div className="h-fit w-full bg-amber-500 text-2xl text-white text-center rounded-2xl shadow-xl"></div>
+        <div className="h-fit w-full bg-amber-500 text-2xl text-white text-center rounded-2xl shadow-xl shadow-black"></div>
     )
 }
