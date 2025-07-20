b8e49016c5bbf098f944122cb06aaa646597df8c
nazisharifi2025 <banonazisharifi@gmail.com>
2025-07-20 22:05:39 +0430

    l

--- src/Componints/Desplay.jsx
+++ src/Componints/Desplay.jsx
@@ -1,5 +1,7 @@
 export default function Desplay(){
     return(
-        <div className="w-full h-fit bg-amber-800 text-2xl text-white flex justify-between items-center font-bold"></div>
+        <div className="w-full h-fit bg-amber-800 text-2xl text-white flex justify-between items-center font-bold">
+            <div className="font-bold text-center"></div>
+        </div>
     )
 }
