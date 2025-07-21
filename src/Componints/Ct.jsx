99ab3ece8c29b7b091e46f703df38219802c6e43
nazisharifi2025 <banonazisharifi@gmail.com>
2025-07-21 23:31:11 +0430

    l

--- src/Componints/Ct.jsx
+++ src/Componints/Ct.jsx
@@ -1,5 +1,5 @@
 export default function Ct (){
     return(
-        <div className="h-screen w-full"></div>
+        <div className="h-screen w-full bg-amber-300 text-white text-2xl"></div>
     )
 }
