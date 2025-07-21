acd91e7281cddd504f00565fae68e84db986e3e1
nazisharifi2025 <banonazisharifi@gmail.com>
2025-07-21 23:42:53 +0430

    l

--- src/Componints/Ct.jsx
+++ src/Componints/Ct.jsx
@@ -1,7 +1,7 @@
 export default function Ct (){
     return(
         <div className="h-screen w-full bg-amber-300 text-white text-2xl text-center font-bold flex  items-center justify-center">
-            <div className="h-[90%] w-[90%]"></div>
+            <div className="h-[90%] w-[90%] bg-pink-500"></div>
         </div>
     )
 }
