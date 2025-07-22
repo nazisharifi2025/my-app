fbcde542f6c546736445fb397b9022fac445f24d
nazisharifi2025 <banonazisharifi@gmail.com>
2025-07-22 21:17:39 +0430

    l

--- src/Componints/Ct.jsx
+++ src/Componints/Ct.jsx
@@ -3,7 +3,7 @@ export default function Ct (){
         <div className="h-screen w-full bg-amber-300 text-white text-2xl text-center font-bold flex  items-center justify-center">
             <div className="h-[90%] w-[90%] bg-pink-500 text-white font-bold text-2xl text-center flex justify-center items-center">
                 <div className="h-[90%] w-[90%] bg-blue-400 text-white flex justify-center font-bold text-2xl items-center text-center">
-                    <div className="h-[90%] w-[90%] bg-cyan-900 text-white text-2xl"></div>
+                    <div className="h-[90%] w-[90%] bg-cyan-900 text-white text-2xl flex justify-center"></div>
                 </div>
             </div>
         </div>
