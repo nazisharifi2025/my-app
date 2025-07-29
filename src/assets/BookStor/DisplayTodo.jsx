3369c77a8e0a10945506cf7a651d86a95681096d
nazisharifi2025 <banonazisharifi@gmail.com>
2025-07-29 22:17:00 +0430

    l

--- src/assets/BookStor/DisplayTodo.jsx
+++ src/assets/BookStor/DisplayTodo.jsx
@@ -1,5 +1,7 @@
 export default function DisplayTodo(){
     return(
-        <div className="h-fit w-full flex flex-col justify-center items-center"></div>
+        <div className="h-fit w-full flex flex-col justify-center items-center ">
+            <div className="w-[50%] h-fit flex"></div>
+        </div>
     )
 }
