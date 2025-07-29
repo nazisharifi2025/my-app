ba2c342004ce2e7be31ab45d1aefeed6dc779214
nazisharifi2025 <banonazisharifi@gmail.com>
2025-07-29 22:03:07 +0430

    l

--- src/assets/BookStor/Form.jsx
+++ src/assets/BookStor/Form.jsx
@@ -1,9 +1,11 @@
+import Form2 from "../../Componints/Form2"
 export default function Form(){
     return(
         <div className="w-full h-screen">
         <div className="h-20 w-full bg-gray-400 shadow-md shadow-gray-900 text-white font-bold flex justify-center items-center">
-            <h1 className="font-bold text-3xl">LIBRARY</h1>
+            <h1 className="font-bold text-4xl">LIBRARY</h1>
         </div>
+        <Form2/>
         </div> 
     )
 }
