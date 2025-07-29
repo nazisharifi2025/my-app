a825c2d2288cb9fb36953e7c4ea5aa7e2ef9cfdc
nazisharifi2025 <banonazisharifi@gmail.com>
2025-07-29 21:51:49 +0430

    l

--- src/assets/BookStor/Form.jsx
+++ src/assets/BookStor/Form.jsx
@@ -1,5 +1,5 @@
 export default function Form(){
     return(
-        <div className="h-20 w-full bg-gray-400 shadow-md"></div>
+        <div className="h-20 w-full bg-gray-400 shadow-md shadow-gray-900 text-white font-bold"></div>
     )
 }
