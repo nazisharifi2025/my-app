cd6d695c563091531eab7b24cbd43d230d39872b
nazisharifi2025 <banonazisharifi@gmail.com>
2025-07-29 22:24:51 +0430

    l

--- src/assets/BookStor/DisplayTodo.jsx
+++ src/assets/BookStor/DisplayTodo.jsx
@@ -1,12 +1,12 @@
 export default function DisplayTodo(){
     return(
-        <div className="h-fit w-full flex flex-col justify-center items-center ">
-            <div className="w-[50%] h-fit flex justify-between items-center">
-                <div className="flex">
+        <div className="h-fit w-full flex flex-col justify-center my-12 items-center ">
+            <div className="w-[30%] h-fit flex justify-between items-center">
+                <div className="flex gap-4">
                     <input type="checkbox" />
-                    <h1 className="font-bold">BOOK</h1>
+                    <h1 className="font-bold text-2xl">BOOK</h1>
                 </div>
-                <h1>🗑️</h1>
+                <h1 className="text-2xl">🗑️</h1>
             </div>
         </div>
     )
