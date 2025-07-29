70a914000e52b3afb66ffba3d4852dccee6b6577
nazisharifi2025 <banonazisharifi@gmail.com>
2025-07-29 22:19:02 +0430

    l

--- src/assets/BookStor/DisplayTodo.jsx
+++ src/assets/BookStor/DisplayTodo.jsx
@@ -1,7 +1,12 @@
 export default function DisplayTodo(){
     return(
         <div className="h-fit w-full flex flex-col justify-center items-center ">
-            <div className="w-[50%] h-fit flex justify-between items-center"></div>
+            <div className="w-[50%] h-fit flex justify-between items-center">
+                <div className="flex">
+                    <input type="checkbox" />
+                    <h1 className="font-bold">BOOK</h1>
+                </div>
+            </div>
         </div>
     )
 }
