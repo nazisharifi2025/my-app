6c6b0722be97f416b41a1d17d73e50590aee2848
nazisharifi2025 <banonazisharifi@gmail.com>
2025-07-30 22:10:19 +0430

    l

--- src/Componints/Git.jsx
+++ src/Componints/Git.jsx
@@ -1,6 +1,6 @@
 function Git(){
     return(
-        <div className="h-screen w-full bg-linear-330 from-30% from-gray-800"></div>
+        <div className="h-screen w-full bg-linear-330 from-30% from-gray-800 to-50% to-gray-400"></div>
     )
 }
 export default Git;
