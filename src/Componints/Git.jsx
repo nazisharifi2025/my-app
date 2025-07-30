4a082f37f8a190c4e8ded36e81a395e2a25327e7
nazisharifi2025 <banonazisharifi@gmail.com>
2025-07-30 22:14:18 +0430

    l

--- src/Componints/Git.jsx
+++ src/Componints/Git.jsx
@@ -1,7 +1,7 @@
 function Git(){
     return(
         <div className="h-screen w-full bg-linear-330 from-30% from-gray-800 to-50% to-gray-400 via-30% via-gray-100 flex justify-center items-center text-2xl font-bold text-gray-400">
-        <div className="h-screen w-full bg-linear-330 from-30% from-gray-800 to-50% to-gray-400 via-30% via-gray-100 flex justify-center items-center text-2xl "></div>
+        <div className="h-screen w-full bg-linear-330 from-30% from-gray-800 to-50% to-gray-400 via-30% via-gray-100 flex justify-center items-center text-2xl font-bold text-gray-400"></div>
         </div>
     )
 }
