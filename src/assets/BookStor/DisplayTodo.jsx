186921f4d5d44bb5f4faf5ff198352dd1aaaaac5
nazisharifi2025 <banonazisharifi@gmail.com>
2025-07-31 21:42:20 +0430

    l

--- src/assets/BookStor/DisplayTodo.jsx
+++ src/assets/BookStor/DisplayTodo.jsx
@@ -1,14 +1,16 @@
 
-export default function DisplayTodo({todos,settodos,items}){
+export default function DisplayTodo({todo,settodos,items}){
     function hanelClick(name){
-          
+        const newTodos = items.map(item=>{
+          return  item.todo === name ? {...item , done:true}: item;
+          })
     }
     return(
         <div className="h-fit w-full flex flex-col justify-center my-12 items-center ">
             <div className="w-[30%] h-fit flex justify-between items-center">
                 <div className="flex gap-4">
-                    <input type="checkbox" checked={props.todo.done} onClick ={()=>hanelClick(props.todo.todo)} className="form-control h-4 w-4 " />
-                    <h1 className="font-bold text-2xl">{props.todo.todo}</h1>
+                    <input type="checkbox"  onClick ={()=>hanelClick(todo.todo)} className="form-control h-4 w-4 " />
+                    <h1 className="font-bold text-2xl">{todo.todo}</h1>
                 </div>
                 <h1 className="text-2xl">🗑️</h1>
             </div>
