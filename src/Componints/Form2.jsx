f7a933d27723af05ed2cff284a88956e92ce767d
nazisharifi2025 <banonazisharifi@gmail.com>
2025-07-31 12:03:37 +0430

    l

--- src/Componints/Form2.jsx
+++ src/Componints/Form2.jsx
@@ -1,15 +1,14 @@
 import { useState } from "react"
-import DisplayTodo from "../assets/BookStor/DisplayTodo";
+// import DisplayTodo from "../assets/BookStor/DisplayTodo";
 import Formf from "../assets/BookStor/Formf.jsx";
+import Todo from "../assets/BookStor/Todo.jsx";
 export default function(){
     
     
     return(
         <div className="h-screen w-full flex flex-col justify-center items-center">
             <Formf/>
-            {todos.map(item=>{
-               return <h1 className="bg-white px-3 py-2 rounded-md text-2xl font-bold text-center mx-auto" key={item}>{item}</h1>
-            })}
+            <Todo/>
           <DisplayTodo/>
         </div>
     )
