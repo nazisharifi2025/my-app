67dc08a405859e2a58158364167a0359e73a88ef
nazisharifi2025 <banonazisharifi@gmail.com>
2025-07-31 11:40:58 +0430

    l

---
+++ src/assets/BookStor/Todo.jsx
@@ -0,0 +1,9 @@
+export default function Todo(){
+    return(
+        <div>
+        {todos.map(item=>{
+               return <h1 className="bg-white px-3 py-2 rounded-md text-2xl font-bold text-center mx-auto" key={item}>{item}</h1>
+            })}
+            </div>
+    )
+}
