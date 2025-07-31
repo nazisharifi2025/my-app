36cb66d02bf68ea709303803d5af22eaa5fcdbc4
nazisharifi2025 <banonazisharifi@gmail.com>
2025-07-31 22:14:20 +0430

    l

--- src/Componints/Form2.jsx
+++ src/Componints/Form2.jsx
@@ -4,7 +4,9 @@ import Formf from "../assets/BookStor/Formf.jsx";
 import Todo from "../assets/BookStor/Todo.jsx";
 export default function(){
     const [todos , settodos]= useState([]);
-    todos.
+   const newTodos=  todos.filter(item=>{
+      return  item.done === true ;
+    })
     return(
         <div className="h-screen w-full flex flex-col justify-center items-center">
             <Formf todos = {todos} settodos= {settodos}/>
@@ -12,7 +14,7 @@ export default function(){
             {/* <Desplay/> */}
                 <div className="w-full absolute bottom-0 left-0 h-fit bg-gray-400 text-2xl px-2 right-0 text-white flex justify-between items-center font-bold">
              <div className="font-bold text-center">ALL Tasks {todos.length}</div>
-            <div className="font-bold text-center">Count in 0</div>
+            <div className="font-bold text-center">Complited Task {newTodos.length}</div>
         </div>
         </div>
     )
