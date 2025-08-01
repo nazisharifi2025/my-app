a866f305026bbacd8cf4512ed59132852bc044e4
nazisharifi2025 <banonazisharifi@gmail.com>
2025-08-01 18:34:36 +0430

    l

--- src/Componints/Commind.jsx
+++ src/Componints/Commind.jsx
@@ -1,5 +1,5 @@
 export default function(){
     return(
-        <div className="h-20 w-20 bg-gray-500 text-white"></div>
+        <div className="h-20 w-20 bg-gray-500 text-white text-2xl font-bold"></div>
     )
 }
