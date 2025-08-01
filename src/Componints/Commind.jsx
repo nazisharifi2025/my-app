e1ed651c967fc5e5e06a3a05e0d564e28931c3c9
nazisharifi2025 <banonazisharifi@gmail.com>
2025-08-01 18:54:26 +0430

    l

--- src/Componints/Commind.jsx
+++ src/Componints/Commind.jsx
@@ -1,7 +1,7 @@
 export default function(){
     return(
         <div className="h-34 w-34 bg-gray-500 text-white text-2xl font-bold text-center absolute right-0 top-24">
-            <h1 className="p-4">Books are friends that never leave your side.</h1>
+            <h1 className="p-4 text-xl text-white">Books are friends that never leave your side.</h1>
         </div>
     )
 }
