6e98e20127f232b4112eb0a4d9378d9e8f113859
nazisharifi2025 <banonazisharifi@gmail.com>
2025-08-02 21:56:17 +0430

    l

--- src/Componints/Jumla.jsx
+++ src/Componints/Jumla.jsx
@@ -1,7 +1,9 @@
 export default function(){
     return(
         <div className="h-screen w-full bg-linear-180 from-40% from-gray-700 to-55% to-white via-30% via-blue-100 text-blue-600 flex justify-center items-center gap-11">
-            <div className="h-[80%] w-[50%] border border-black shadow-md shadow-gray-900 flex justify-center text-2xl text-center text-white gap-6"></div>
+            <div className="h-[80%] w-[50%] border border-black shadow-md shadow-gray-900 flex justify-center text-2xl text-center text-white gap-6">
+                <h1>FATHER</h1>
+            </div>
         </div>
     )
 }
