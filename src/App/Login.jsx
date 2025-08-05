6f76e467bf0bebb4d736948b20aff18663fe410c
nazisharifi2025 <banonazisharifi@gmail.com>
2025-08-05 20:43:09 +0430

    l

--- src/App/Login.jsx
+++ src/App/Login.jsx
@@ -1,7 +1,7 @@
 export default function Login(){
     return(
         <div >
-            <form action="" className="h-fit w-[45%] border rounded-2xl flex flex-col gap-3 ">
+            <form action="" className="h-fit w-[45%] border rounded-2xl flex flex-col gap-3 p-4">
                 <input type="text" placeholder="Inter User name" className="border rounded-2xl py-2 px-8" />
             </form>
         </div>
