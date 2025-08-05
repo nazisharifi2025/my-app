5103add78b04aebb3f99f9b5e8c2ad3636ff3568
nazisharifi2025 <banonazisharifi@gmail.com>
2025-08-05 20:39:45 +0430

    l

--- src/App/Login.jsx
+++ src/App/Login.jsx
@@ -1,6 +1,6 @@
 export default function Login(){
     return(
-        <div className="h-fit w-[45%] border rounded-2xl flex flex-col">
+        <div className="h-fit w-[45%] border rounded-2xl flex flex-col gap-3 ">
 
         </div>
     )
