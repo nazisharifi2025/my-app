851f34f1c1ec4995ad68913b8e43e306fe06a689
nazisharifi2025 <banonazisharifi@gmail.com>
2025-08-05 20:45:47 +0430

    l

--- src/App/Login.jsx
+++ src/App/Login.jsx
@@ -8,7 +8,7 @@ export default function Login(){
                 </div>
                 <div>
                     {/* <label htmlFor="">User Name</label> */}
-                <input type="text" placeholder="Inter User name" />
+                <input type="text" placeholder="Inter User name"  className="border rounded-2xl " />
                 </div>
             </form>
         </div>
