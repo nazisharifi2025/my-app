6be8be75eddbf27e4f2ade9b231fe8340ab27085
Nazi <banonazisharifi@gmail.com>
2025-08-10 21:48:10 +0430

    input added

--- src/Componints/LogIn.jsx
+++ src/Componints/LogIn.jsx
@@ -3,6 +3,7 @@ export default function LogIn(){
         <div class="h-[80%] w-40% mx-auto border border-gray-400 rounded-2xl flex flex-col gap-2.5 ">
             <h1 class="font-bold text-3xl">LOG IN</h1>
             <input type="text" class="py-2 px-4 w-full bg-gray-300 shadow-md shadow-black" />
+            <input type="text" />
         </div>
     )
 }
