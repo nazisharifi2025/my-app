0e57c8e2aff94763261823a336c4a14419f3d22e
Nazi <banonazisharifi@gmail.com>
2025-08-10 21:38:36 +0430

    Some Styel added

--- src/Componints/LogIn.jsx
+++ src/Componints/LogIn.jsx
@@ -1,6 +1,6 @@
 export default function LogIn(){
     return(
-        <div class="h-[80%] w-40% mx-auto">
+        <div class="h-[80%] w-40% mx-auto border border-gray-400 rounded-2xl">
         
         </div>
     )
