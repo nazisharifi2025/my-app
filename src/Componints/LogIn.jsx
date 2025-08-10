54dedefd02a958846738ac3e3732238eb7ce6121
Nazi <banonazisharifi@gmail.com>
2025-08-10 21:51:29 +0430

    Some Styel added

--- src/Componints/LogIn.jsx
+++ src/Componints/LogIn.jsx
@@ -4,7 +4,7 @@ export default function LogIn(){
             <h1 class="font-bold text-3xl">LOG IN</h1>
             <input type="text" class="py-2 px-4 w-full bg-gray-300 shadow-md shadow-black" />
             <input type="text"  class="py-2 px-4 w-full bg-gray-300 shadow-md shadow-black"/>
-            <div class="font-bold text-xl flex items-center">
+            <div class="font-bold text-xl flex items-center justify-between">
                 <h1 class="">FOrget your password?</h1>
                 <h1 class="">Creat new acount</h1>
             </div>
