fde5ef7ef800454c847df705b265d1f90212ad9a
Nazi <banonazisharifi@gmail.com>
2025-08-10 21:52:51 +0430

    some style for button added

--- src/Componints/LogIn.jsx
+++ src/Componints/LogIn.jsx
@@ -8,7 +8,7 @@ export default function LogIn(){
                 <h1 class="">FOrget your password?</h1>
                 <h1 class="">Creat new acount</h1>
             </div>
-            <button>Send</button>
+            <button class="py-2 px-4 bg-blue-100">Send</button>
         </div>
     )
 }
