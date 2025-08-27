51e261e0cb59697fe538dcb6473154fed10c667e
Nazi <banonazisharifi@gmail.com>
2025-08-27 12:28:48 +0430

    Futcher_pro add

---
+++ src/Componints/bo.jsx
@@ -0,0 +1,49 @@
+import bad from "../assets/img/bad.jpg";
+import fran from "../assets/img/fran.jpg";
+import bland from "../assets/img/bland.jpg";
+import talar from "../assets/img/talar.jpg";
+import muhakima from "../assets/img/muhakima.jpg";
+import salar from "../assets/img/salar.jpg";
+import Bok from "./Bok"
+export default function Book(){
+     const book =[
+        {
+            name : "بلندی های بادگیر",
+            imgUrl : bland,
+            price : 180
+        },
+         {
+            name : "فرانکنشتاین",
+            imgUrl : fran,
+            price : 200
+        },
+         {
+            name : "بادبدک باز",
+            imgUrl : bad,
+            price : 150
+        },
+         {
+            name : "تالار گرگ",
+            imgUrl : talar,
+            price : 100
+        },
+          {
+            name : "محاکمه",
+            imgUrl : muhakima,
+            price : 180
+        },
+          {
+            name : "سالار مگس ها",
+            imgUrl : salar,
+            price : 170
+        },
+        ]
+    return (
+       
+        <div className="h-screen w-full bg-gray-300 flex justify-between gap-6 overflow-y-scroll p-6 flex-wrap font-bold">
+            {book.map(item=>(
+                <Bok name ={item.name} imgUrl = {item.imgUrl} price = {item.price}/>
+            ))}
+        </div>
+    )
+}
--- src/PageStyle.jsx/Componint/Header.jsx
+++ src/PageStyle.jsx/Componint/Header.jsx
@@ -1,5 +1,7 @@
 export default function Header(){
     return(
-        <div class="w-full h-20 flex justify-between items-center"></div>
+        <div class="w-full h-20 flex justify-between items-center">
+            <img src="../public/img/logo.webp" alt="" class="h-[90%]" />
+        </div>
     )
 }
