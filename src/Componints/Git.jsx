a1bf6f60b34ff0eb84e7939fabf72fb1e98f27bc
Nazi <banonazisharifi@gmail.com>
2025-09-01 22:36:32 +0430

    Product componint add

--- src/Componints/Git.jsx
+++ src/Componints/Git.jsx
@@ -3,6 +3,7 @@ function Git(){
         <div className="h-screen w-full bg-linear-330 from-30% from-gray-800 to-50% to-gray-400 via-30% via-gray-100 flex justify-center items-center text-2xl font-bold text-gray-400">
         <div className="h-screen w-full bg-linear-330 from-30% from-gray-800 to-50% to-gray-400 via-30% via-gray-100 flex justify-center items-center text-2xl font-bold text-gray-400"></div>
         <div className="h-screen w-full bg-linear-330 from-30% from-gray-800 to-50% to-gray-400 via-30%  via-gray-100 flex justify-center items-center text-2xl font-bold text-gray-400" ></div>
+        <div className="h-screen w-full bg-linear-330 from-30% from-gray-800 to-50% to-gray-400 via-30%  via-gray-100 flex justify-center items-center text-2xl font-bold text-gray-400" ></div>
         </div>
     )
 }
